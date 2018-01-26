bcb7b36a54309b5967598db495118f1ab3757dfd
sheyla fuentes <sgf763@mail.missouri.edu>
2018-01-25 23:05:29 -0600

    first commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by Sheyla Astete on 1/25/18.
+//  Copyright © 2018 tech inovator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
